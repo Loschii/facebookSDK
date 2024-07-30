bba16165084b0487575f8c87fb25555c6c44f347
Loschii <bruno.loschi@outlook.com>
2024-07-30 12:20:43 -0300

    att informações login google

--- script.js
+++ script.js
@@ -78,9 +78,9 @@ document.getElementById('fb-logout-btn').addEventListener('click', function() {
 function handleCredentialResponse(response) {
     const responsePayload = decodeJwtResponse(response.credential);
     console.log('ID: ' + responsePayload.sub);
-    console.log('Full Name: ' + responsePayload.name);
-    console.log('Given Name: ' + responsePayload.given_name);
-    console.log('Family Name: ' + responsePayload.family_name);
+    console.log('Nome completo: ' + responsePayload.name);
+    console.log('Primeiro nome: ' + responsePayload.given_name);
+    console.log('Sobrenome: ' + responsePayload.family_name);
     console.log('Image URL: ' + responsePayload.picture);
     console.log('Email: ' + responsePayload.email);
 
